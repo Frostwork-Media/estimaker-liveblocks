10819295e0aa9f3e891095664c6da27b959e4a39
Rob Gordon <rob.gordon@tone-row.com>
2023-09-07 15:21:43 -0400

    Add squiggle code icon

--- app/src/components/NodeValue.tsx
+++ app/src/components/NodeValue.tsx
@@ -1,7 +1,7 @@
 import { useMutation, useStorage } from "../liveblocks.config";
 
 const INPUT_CLASSES =
-  "border-2 py-1 px-2 font-mono rounded text-neutral-600 text-sm w-full h-8";
+  "py-1 px-2 font-mono rounded-r text-neutral-600 text-sm w-full h-8 grow focus:outline-none";
 
 export function NodeValue({ nodeId }: { nodeId: string }) {
   const node = useStorage((state) => state.nodes.get(nodeId));
@@ -12,16 +12,21 @@ export function NodeValue({ nodeId }: { nodeId: string }) {
     node.set("value", value);
   }, []);
   return (
-    <input
-      className={INPUT_CLASSES}
-      type="text"
-      value={node?.value ?? ""}
-      name="value"
-      onChange={(e) => {
-        const value = e.target.value;
-        setNodeValue(value);
-      }}
-    />
+    <div className="flex group rounded border-2 focus-within:border-slate-300">
+      <div className="bg-slate-200 flex items-center justify-center w-8 h-8 border-r-2 group-focus-within:bg-slate-300 group-focus-within:border-r-slate-300">
+        <img src="/squiggle-logo.png" className="w-4 h-4 black-and-white" />
+      </div>
+      <input
+        className={INPUT_CLASSES}
+        type="text"
+        value={node?.value ?? ""}
+        name="value"
+        onChange={(e) => {
+          const value = e.target.value;
+          setNodeValue(value);
+        }}
+      />
+    </div>
   );
 }
 
